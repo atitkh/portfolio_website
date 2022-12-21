dfd40de50be128bd2231bf910b2c3caadc7b1220
Atit Kharel <atit286@gmail.com>
2022-12-21 17:33:09 +0545

    added scroll bar

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,4 @@
 import { React, useEffect, useState } from 'react';
-import { NavLink } from "react-router-dom";
 import './home.css';
 import { Gallery } from '../../components';
 import axios from 'axios';
