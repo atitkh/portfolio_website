9fadc54c8c392fbd99a71a2aca48adeb28d6eb17
Atit Kharel <atit286@gmail.com>
2023-01-02 16:11:47 +0545

    mac categories fix

--- src/pages/404/404.jsx
+++ src/pages/404/404.jsx
@@ -5,7 +5,7 @@ import './404.css';
 function NoPage() {
     return (
         <div id="main">
-            <div class="fof">
+            <div className="fof">
                     <h1>Error 404</h1>
                     <p>The page you are looking for does not exist.</p>
             </div>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -29,15 +29,15 @@ function Home() {
     if (loading) {
         return (
             <div className="loader-container">
-                <div class="wrapper">
-                    <span class="circle circle-1"></span>
-                    <span class="circle circle-2"></span>
-                    <span class="circle circle-3"></span>
-                    <span class="circle circle-4"></span>
-                    <span class="circle circle-5"></span>
-                    <span class="circle circle-6"></span>
-                    <span class="circle circle-7"></span>
-                    <span class="circle circle-8"></span>
+                <div className="wrapper">
+                    <span className="circle circle-1"></span>
+                    <span className="circle circle-2"></span>
+                    <span className="circle circle-3"></span>
+                    <span className="circle circle-4"></span>
+                    <span className="circle circle-5"></span>
+                    <span className="circle circle-6"></span>
+                    <span className="circle circle-7"></span>
+                    <span className="circle circle-8"></span>
                 </div>
             </div>
         );
