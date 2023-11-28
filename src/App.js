c00143c8692d45849df2e3b33bb9cf3d797cb247
Atit Kharel <atit286@gmail.com>
2023-11-28 17:56:11 +0545

    [update] hashrouter for project view

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import React from 'react';
 import { MantineProvider } from '@mantine/core';
 import './App.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
 import { Home, NoPage, SnapAR, Vr } from "./pages";
 
 const App = () => {
   return (
+    <MantineProvider theme={{ primaryColor: 'dark' }}>
     <BrowserRouter>
-      <MantineProvider theme={{ primaryColor: 'dark' }}>
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/view/:id" element={<Home />} />
-          <Route exact path="/vr" element={<Vr />} />
-          <Route exact path="/snapar/:lensID" element={<SnapAR />} />
-          <Route path="*" element={<NoPage />} />
-        </Routes>
-      </MantineProvider>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/vr" element={<Vr />} />
+        <Route exact path="/snapar/:lensID" element={<SnapAR />} />
+        <Route path="*" element={<NoPage />} />
+      </Routes>
     </BrowserRouter>
+    <HashRouter>
+      <Routes>
+        <Route exact path="/view/:id" element={<Home />} />
+      </Routes>
+    </HashRouter>
+  </MantineProvider>
   );
 }
 
