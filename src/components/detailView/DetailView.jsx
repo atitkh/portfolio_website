7dffe0686eda690b07bc52afd13328f482bc68ee
Atit Kharel <atit286@gmail.com>
2024-09-17 01:16:55 -0500

    update: enhance hyperlink handling in markdown and improve detail view styling

--- src/components/detailView/DetailView.jsx
+++ src/components/detailView/DetailView.jsx
@@ -16,8 +16,24 @@ function DetailView() {
         useEffect(() => {
                 const fetchData = async () => {
                         const result = await axios.get(`https://api.atitkharel.com.np/portfolio/atit/md?id=${id}`);
-                        setMarkdown(result.data);
+                        let data = result.data;
 
+                        // Go through the data and replace hyperlinks with actual links, excluding image links
+                        data = data.replace(/(\[.*?\]\(.*?\))/g, (match, p1, offset, string) => {
+                                const prevChar = string.charAt(offset - 1);
+                                const text = match.match(/\[.*?\]/)[0].replace(/^\[|\]$/g, '');
+                                const link = match.match(/\(.*?\)/)[0].replace(/^\(|\)$/g, '');
+
+                                if (prevChar === '!') {
+                                        return match; // It's an image link, return as is
+                                }
+
+                                // Return as an HTML link
+                                return `<a href="${link}" target="_blank" rel="noopener noreferrer" style="text-decoration:solid; color: #33beff; font-weight: bold;
+                                ">${text}</a>`;
+                        });
+                        setMarkdown(data);
+                        
                         setLoading(false);
                 }
                 fetchData();
@@ -34,8 +50,17 @@ function DetailView() {
 
                         <div className='detail'>
                                 <div className='detail_body'>
-                                        <Container c='#282828'>
-                                                <Markdown className='detail_markdown' rehypePlugins={[rehypeRaw]} remarkPlugins={[remarkGfm]}>{markdown}</Markdown>
+                                        <Container c='#282828' style={{
+                                                fontSize: '1.2rem',
+                                                lineHeight: '1.8',
+                                        }} >
+                                                <Markdown
+                                                        className='detail_markdown'
+                                                        rehypePlugins={[rehypeRaw]}
+                                                        remarkPlugins={[remarkGfm]}
+                                                >
+                                                        {markdown}
+                                                </Markdown>
                                         </Container>
                                 </div>
                         </div>
